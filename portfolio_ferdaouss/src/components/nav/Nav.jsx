886a1a09503120a8469d7d2c095c0a723421632d
fadlani <fadlani@sevenpark.fr>
2024-03-14 14:13:22 +0100

    added sections 1 and 2

--- portfolio_ferdaouss/src/components/nav/Nav.jsx
+++ portfolio_ferdaouss/src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './nav.css'
+import {IoMdHome} from 'react-icons/io'
+import {LuUser2} from 'react-icons/lu'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {TbMessage} from 'react-icons/tb'
+import { useState } from 'react'
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav ] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><IoMdHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><LuUser2/></a>
+      <a href='#experience'  onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href='#services'  onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href='#contact'  onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TbMessage/></a>
+    </nav>
   )
 }
 
